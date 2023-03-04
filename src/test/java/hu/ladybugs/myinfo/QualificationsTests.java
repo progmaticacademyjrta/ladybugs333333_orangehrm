1ef138de4104ae7f962f049980ada71bb4e2f6fb
csizmatimea <csizmatimea@gmail.com>
2023-03-04 11:14:49 +0100

    Delete pages

--- src/test/java/hu/ladybugs/myinfo/QualificationsTests.java
+++
@@ -1,8 +0,0 @@
-package hu.ladybugs.myinfo;
-
-import hu.ladybugs.driverfactory.DriverBaseTest;
-import hu.ladybugs.myinfo.pages.QualificationsPage;
-
-public class QualificationsTests extends DriverBaseTest {
-    QualificationsPage QualificationsPage;
-}
